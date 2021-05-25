9ca33186f4ef4fc5a593881739f4ea5e7398f016
vvadikk <faqproshival@yandex.ru>
2021-05-25 18:38:02 +0300

    intiate and final

---
+++ src/main/java/Bot.java
@@ -0,0 +1,37 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class Bot extends TelegramLongPollingBot {
+        @Override
+        public void onUpdateReceived(Update update) {
+                // We check if the update has a message and the message has text
+                if (update.hasMessage() && update.getMessage().hasText()) {
+                        SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+                        message.setChatId(update.getMessage().getChatId().toString());
+                        message.setText(update.getMessage().getText());
+
+                        try {
+                                execute(message); // Call method to send the message
+                        } catch (TelegramApiException e) {
+                                e.printStackTrace();
+                        }
+                }
+        }
+
+        @Override
+        public String getBotUsername() {
+                return "verygodbot";
+        }
+
+
+        @Override
+        public String getBotToken() {
+                return Secret.SECRET_KEY;
+        }
+
+
+
+}
